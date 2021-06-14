024fc3511b12bfed0c2b0ae7e9f1d71c7a6dc387
Tgx Bots <82932839+TgxBots@users.noreply.github.com>
2021-06-14 15:43:02 +0530

    Create spam.py

---
+++ SpamBot/plugins/spam.py
@@ -0,0 +1,66 @@
+from .. import nora, cmd
+from pyrogram import filters
+from time import time
+from pyrogram.types import (
+   InlineKeyboardMarkup, 
+   InlineKeyboardButton,
+   ChatPermissions
+   Message,
+   CallbackQuery
+)
+
+from SpamBot.plugins.admin import list_admins
+
+DB = {}
+DEVS = [1704673514, 1157157702] 
+
+@nora.on_message(
+        ~filters.service
+        & ~filters.me
+        & ~filters.private
+        & ~filters.channel
+)
+async def flood(client, message):
+    if not message.from_user:
+        return
+    user_id = message.from_user.id
+    mention = message.from_user.mention
+    chat_id = message.chat.id
+
+    mods = (await list_admins(chat_id)) + DEVS
+    if user_id in mods:
+        return
+
+    if chat_id not in DB:
+        DB[chat_id] = {}
+    if user_id not in DB[chat_id]:
+        DB[chat_id][user_id] = 0
+    
+    if DB[chat_id][user_id] >= 10:
+        DB[chat_id][user_id] = 0
+        try:
+            await message.chat.restrict_member(
+                    user_id,
+                    permissions=ChatPermissions(),
+                    until_date=int(time() + 3600)
+                    )
+        except Exception:
+            return
+    text = f"""
+**⚠️ Spam-Detection ⚠️**
+
+**User:** {mention}
+**Chat:** `{chat_id}`
+**Messages:** `10`
+
+__Muted {mention} for 1 hour
+as he tried to spam the chat!__
+"""
+    keyboard = [
+     InlineKeyboardButton("Unmute", callback_data=f"unmute_{user_id}")
+    ]
+    await message.reply(text, reply_markup=InlineKeyboardMarkup([keyboard]))
+
+    DB[chat_id][user_id] += 1
+
+    
