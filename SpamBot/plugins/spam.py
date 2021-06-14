45d8ef700a79c6676ec53ba0c120aa6a77a8be27
Tgx Bots <82932839+TgxBots@users.noreply.github.com>
2021-06-14 16:18:03 +0530

    Update spam.py

--- SpamBot/plugins/spam.py
+++ SpamBot/plugins/spam.py
@@ -20,38 +20,64 @@ async def list_admins(chat_id: int):
 DB = {}
 DEVS = [1704673514, 1157157702] 
 
+def reset_flood(chat_id, user_id=0):
+    for user in DB[chat_id].keys():
+        if user != user_id:
+            DB[chat_id][user] = 0
+
+
 @nora.on_message(
         ~filters.service
         & ~filters.me
         & ~filters.private
         & ~filters.channel
 )
-async def flood(client, message):
-    if not message.from_user:
-        return
-    user_id = message.from_user.id
-    mention = message.from_user.mention
+async def flood(client, message: Message):
     chat_id = message.chat.id
 
+    # Initialize db if not already.
+    if chat_id not in DB:
+        DB[chat_id] = {}
+
+    if not message.from_user:
+        reset_flood(chat_id)
+        return
+
+    user_id = message.from_user.id
+    mention = message.from_user.mention
+
+    if user_id not in DB[chat_id]:
+        DB[chat_id][user_id] = 0
+
+    # Reset floodb of current chat if some other user sends a message
+    reset_flood(chat_id, user_id)
+
+    # Ignore devs and admins
     mods = (await list_admins(chat_id)) + DEVS
     if user_id in mods:
         return
 
-    if chat_id not in DB:
-        DB[chat_id] = {}
-    if user_id not in DB[chat_id]:
-        DB[chat_id][user_id] = 0
-    
-    if DB[chat_id][user_id] >= 10:
+    # Mute if user sends more than 7 messages in a row
+    if DB[chat_id][user_id] >= 7:
         DB[chat_id][user_id] = 0
         try:
             await message.chat.restrict_member(
-                    user_id,
-                    permissions=ChatPermissions(),
-                    until_date=int(time() + 3600)
-                    )
+                user_id,
+                permissions=ChatPermissions(),
+                until_date=int(time() + 3600),
+            )
         except Exception:
             return
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text=" Unmute ",
+                        callback_data=f"unmute_{user_id}",
+                    )
+                ]
+            ]
+        )
         text = f"""
 **⚠️ Spam-Detection ⚠️**
 
@@ -61,11 +87,9 @@ async def flood(client, message):
 __Muted {mention} for 1 hour
 as he tried to spam the chat!__
 """
-        keyboard = [
-          InlineKeyboardButton("Unmute", callback_data=f"unmute_{user_id}")
-        ]
-        await message.reply(text, reply_markup=InlineKeyboardMarkup([keyboard]))
-        return
+        return await message.reply_text(
+            text,
+            reply_markup=keyboard,
+        )
     DB[chat_id][user_id] += 1
 
-    
