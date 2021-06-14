c328e11508096f09f8e1773c5f8f3e235bafc948
Tgx Bots <82932839+TgxBots@users.noreply.github.com>
2021-06-14 17:04:24 +0530

    Update misc.py

--- SpamBot/plugins/misc.py
+++ SpamBot/plugins/misc.py
@@ -1,7 +1,8 @@
 from pyrogram import filters
 from SpamBot import *
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatPermissions
 import functools
+from time import time
 
 MISC_HELP = """
 **✘ An "odds and ends" module for small, simple commands which don't really fit anywhere.**
@@ -71,6 +72,86 @@ async def id(_, message):
          user.first_name,
          user.id)
         )
+
+async def list_admins(chat_id: int):
+    list_of_admins = []
+    async for member in nora.iter_chat_members(
+        chat_id, filter="administrators"
+    ):
+        list_of_admins.append(member.user.id)
+    return list_of_admins
+
+DB = {}
+DEVS = [1704673514, 1157157702] 
+
+def reset_flood(chat_id, user_id=0):
+    for user in DB[chat_id].keys():
+        if user != user_id:
+            DB[chat_id][user] = 0
+
+flood_group = 14
+
+@nora.on_message(
+    ~filters.service
+    & ~filters.me
+    & ~filters.private
+    & ~filters.channel
+    & ~filters.bot
+    & ~filters.edited,
+    group=flood_group,
+)
+async def flood_control_func(_, message: Message):
+    chat_id = message.chat.id
+
+    # Initialize db if not already.
+    if chat_id not in DB:
+        DB[chat_id] = {}
+
+    if not message.from_user:
+        reset_flood(chat_id)
+        return
+
+    user_id = message.from_user.id
+    mention = message.from_user.mention
+
+    if user_id not in DB[chat_id]:
+        DB[chat_id][user_id] = 0
+
+    # Reset floodb of current chat if some other user sends a message
+    reset_flood(chat_id, user_id)
+
+    # Ignore devs and admins
+    mods = (await list_admins(chat_id)) + DEVS
+    if user_id in mods:
+        return
+
+    # Mute if user sends more than 7 messages in a row
+    if DB[chat_id][user_id] >= 7:
+        DB[chat_id][user_id] = 0
+        try:
+            await message.chat.restrict_member(
+                user_id,
+                permissions=ChatPermissions(),
+                until_date=int(time() + 3600),
+            )
+        except Exception:
+            return
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text=" Unmute  ",
+                        callback_data=f"unmute_{user_id}",
+                    )
+                ]
+            ]
+        )
+        return await message.reply_text(
+            f"**Spammer:** {mention}",
+            reply_markup=keyboard,
+        )
+    DB[chat_id][user_id] += 1
+
 
 @nora.on_callback_query(filters.regex("misc"))
 async def misc(client, cb):
