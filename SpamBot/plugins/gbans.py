4c2fda48d06c33f273adfd2c51ec5a50005a1d74
TgxBots <tgxbots54@yahoo.com>
2021-06-20 16:09:44 +0530

    Oooc

--- SpamBot/plugins/gbans.py
+++ SpamBot/plugins/gbans.py
@@ -23,7 +23,7 @@ MSG = """
 @nora.on_message(cmd("gban"))
 async def gban_(client, message):
     sudos = await get_all_sudos()
-    mods = sudos + int(1704673514)
+    mods = int(sudos) + 1704673514
     if message.from_user.id not in mods:
         return await message.reply(
                 "You cant use this cmd!"
