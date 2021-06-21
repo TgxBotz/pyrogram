66b3f23e6e40213d923d6d0281e973af484fd7b1
TgxBots <tgxbots54@yahoo.com>
2021-06-21 13:15:45 +0530

    Ping Fixed

--- SpamBot/plugins/ping.py
+++ SpamBot/plugins/ping.py
@@ -7,7 +7,8 @@ from .. import nora, cmd
 @nora.on_message(cmd("ping"))
 async def ping(client, message):
     start = datetime.now()
+    msg = await message.reply("`Pinging..........`")
     uptime = get_readable_time((time.time() - START_TIME))
     end = datetime.now()
     du = (end - start).microseconds / 1000
-    await message.reply(f"**Pong:** __{du}__\n**Uptime:** __{uptime}__")
+    await msg.edit(f"**Pong:** __{du}__\n**Uptime:** __{uptime}__")
